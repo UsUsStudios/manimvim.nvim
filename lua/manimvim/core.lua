900bd616e1f1982ae527634992cd62719b3c3b5d
UsUsStudios <ususstudios1@gmail.com>
2026-02-22 10:42:40 -0700

    last picked selection

--- lua/manimvim/core.lua
+++ lua/manimvim/core.lua
@@ -1,11 +1,21 @@
 local M = {}
 
+local previous = ""
+
 function M.render()
-	vim.ui.select({ "hello", "hi" }, {
+	local scenes = { "hi", "hello" }
+	if previous ~= "" then
+		table.insert(scenes, 1, previous .. " (last picked)")
+	end
+
+	vim.ui.select(scenes, {
 		prompt = "Select a scene to render: ",
 	}, function(choice)
 		if choice then
 			print("selected " .. choice)
+			if choice ~= previous .. " (last picked)" then
+				previous = choice
+			end
 		else
 			print("cancelled")
 		end
